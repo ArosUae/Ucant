a787ae0ecf3c62dddda98792ad6b1675cddc3c55
ArosUae <65586885+ArosUae@users.noreply.github.com>
2020-05-19 09:57:38 +0400

    Add files via upload

---
+++ khalid.js
@@ -0,0 +1,36 @@
+'use strict';
+var _0xf63f = [".nav-tabs", "insertBefore", '<form name="jump">\r\n<div class="MyColors">\r\n\t\t\t\t\t<map id="ColorsMap" name="ColorsMap">\r\n\t\t\t\t\t<area alt="" coords="270, 0, 295, 25" href="javascript:chooseStyle(\'none\',60)" shape="rect" title="1">\r\n\t\t\t\t\t<area alt="" coords="239, 0, 265, 25" href="javascript:chooseStyle(\'Co2\',60)" shape="rect" title="2">\r\n\t\t\t\t\t<area alt="" coords="209, 0, 235, 25" href="javascript:chooseStyle(\'Co3\',60)" shape="rect" title="3">\r\n\t\t\t\t\t<area alt="" coords="178, 0, 203, 25" href="javascript:chooseStyle(\'Co4\',60)" shape="rect" title="4">\r\n\t\t\t\t\t<area alt="" coords="148, 0, 173, 25" href="javascript:chooseStyle(\'Co5\',60)" shape="rect" title="5">\r\n\t\t\t\t\t<area alt="" coords="116, 0, 142, 25" href="javascript:chooseStyle(\'Co6\',60)" shape="rect" title="6">\r\n\t\t\t\t\t<area alt="" coords="87, 0, 110, 25" href="javascript:chooseStyle(\'Co7\',60)" shape="rect" title="7">\r\n\t\t\t\t\t<area alt="" coords="52, 0, 80, 25" href="javascript:chooseStyle(\'Co8\',60)" shape="rect" title="8">\r\n\t\t\t\t\t</map>\r\n\t\t\t <img alt="" height="25" src="https://1.top4top.net/p_1388gqx1c1.gif" width="300" usemap="#ColorsMap"></div>\r\n</form>\r\n\r\n\r\n \r\n\r\n',
+"https://rawcdn.githack.com/waleed-qaid/des/be9e1d25a3f5960e1ab57e2dca7fd843952cdfbe/www.1des.net-chatt-www.s6acl.com-color1.js", "getScript", "1px 0", "1px solid #ffffff", "2px", "5px", "css", ".hand.nosel.fl.uzr.border", ".room.borderg.hand.nosel.fl", "border-radius", ".corner.fa.fa-user.label.label-primary.fr.uc", '<input id="stealth" type="checkbox" value="" maxlength="12"><i class="fa fa-eye-slash" aria-hidden="true">', "html", ".checkbox label", ".loginstat", "insertAfter", "<div class='fr borderg' style='padding:2px;background-color:white;float:left;margin-left:4px;'> \u0627\u0633\u062a\u0627\u064a\u0644\u0627\u062a :<a href='http://www.1des.net/'>\u0648\u0646 \u062f\u064a\u0632\u0627\u064a\u0646</a></div>",
+"title", " \u2665\ufe0f \u0645\u0633\u0640\u0640\u0640\u0627\u062d\u0647 \u062d\u0640\u0640\u0631\u0647 \u0644\u0623\u0628\u0640\u0640\u062f\u0622\u0639\u0627\u062a\u0643\u0640\u0640\u0645 \u2665\ufe0f ", "attr", "[data-target=#wall]", "width", "100%", "fl", "addClass", "body", '<link rel="stylesheet" type="text/css" href="">\r\n\r\n<link href="https://rawcdn.githack.com/waleed-qaid/1des/c08ddf27750d9c14b9d1ee136dbf0052d6fc5c65/www.1des.net-chatt-www.wlllh.com1.css" rel="stylesheet" type="text/css" />\r\n<link href="https://rawcdn.githack.com/waleed-qaid/1des/c08ddf27750d9c14b9d1ee136dbf0052d6fc5c65/www.1des.net-chatt-www.wlllh.com2.css" media="screen" rel="alternate stylesheet" title="Co2" type="text/css" />\r\n<link href="https://rawcdn.githack.com/waleed-qaid/1des/c08ddf27750d9c14b9d1ee136dbf0052d6fc5c65/www.1des.net-chatt-www.wlllh.com3.css" media="screen" rel="alternate stylesheet" title="Co3" type="text/css" />\r\n<link href="https://rawcdn.githack.com/waleed-qaid/1des/c08ddf27750d9c14b9d1ee136dbf0052d6fc5c65/www.1des.net-chatt-www.wlllh.com4.css" media="screen" rel="alternate stylesheet" title="Co4" type="text/css" />\r\n<link href="https://rawcdn.githack.com/waleed-qaid/1des/c08ddf27750d9c14b9d1ee136dbf0052d6fc5c65/www.1des.net-chatt-www.wlllh.com5.css" media="screen" rel="alternate stylesheet" title="Co5" type="text/css" />\r\n<link href="https://rawcdn.githack.com/waleed-qaid/1des/c08ddf27750d9c14b9d1ee136dbf0052d6fc5c65/www.1des.net-chatt-www.wlllh.com6.css" media="screen" rel="alternate stylesheet" title="Co6" type="text/css" />\r\n<link href="https://rawcdn.githack.com/waleed-qaid/1des/c08ddf27750d9c14b9d1ee136dbf0052d6fc5c65/www.1des.net-chatt-www.wlllh.com7.css" media="screen" rel="alternate stylesheet" title="Co7" type="text/css" />\r\n<link href="https://rawcdn.githack.com/waleed-qaid/1des/c08ddf27750d9c14b9d1ee136dbf0052d6fc5c65/www.1des.net-chatt-www.wlllh.com8.css" media="screen" rel="alternate stylesheet" title="Co8" type="text/css" />\r\n<style>\r\n\r\n\r\nlabel.fl.label.loginstat {\r\n margin-top: 0px!important;\r\n transform: matrix(1, 0, 0, 1, 92, 2);\r\n}\r\n\r\n\r\n.MyColors {\r\n position: absolute;\r\n\ttransform:matrix(1, 0, 0, 1, -49, 0);\r\n\tmargin-top: 2px;\r\n width: 300px;\r\n height: 25px;\r\n float: left;\r\n background: transparent url(https://www.wlllh.com/sico/z1e85rtukr10.png) no-repeat scroll right top;\r\n text-align: left;\r\n}\r\n\r\n.fr.borderg, .fr.borderg a {\r\n font-size: 10px!important;\r\n padding: 0 5px!important;\r\n position: relative;\r\n color: #ffffff;\r\n}\r\n.fr.borderg {\r\n border: 0px;\r\n margin-top: 0;\r\n height: 0;\r\n\r\n}\r\n\r\n\r\nlabel.label.label-primary.mini.fl {\r\npadding: 1px;\r\nborder-radius: 3px!important;\r\n}\r\n\r\n.hmsg {background-color: rgb(235, 241, 255)!important;}\r\na.label.label-primary.fl {display: none}\r\n#des1 {margin-bottom: 2px;font-family: \'El Messiri\',FontAwesome!important; background-image: linear-gradient(90deg,#d0d0d0 0,#bababa 10%,#908f8f 51%,#bababa 89%,#d0d0d0 100%);color: #f9f9f9;border-radius: 3px;padding: 4px 0!important;padding-bottom: 3px;}\r\n\r\n</style>\r\n',
+"3px", ".u-pic", "button.btn.btn-primary", ".label-primary"];
+$("" + _0xf63f[2])[_0xf63f[1]](_0xf63f[0]);
+$[_0xf63f[4]](_0xf63f[3]);
+$(_0xf63f[10])[_0xf63f[9]]({
+"margin" : _0xf63f[5],
+borderRight : _0xf63f[6],
+borderLeft : _0xf63f[6],
+borderTop : _0xf63f[6],
+borderBottom : _0xf63f[6],
+paddingTop : _0xf63f[7],
+borderRadius : _0xf63f[8]
+});
+$(_0xf63f[11])[_0xf63f[9]]({
+"margin" : _0xf63f[5],
+borderRight : _0xf63f[6],
+borderLeft : _0xf63f[6],
+borderTop : _0xf63f[6],
+borderBottom : _0xf63f[6],
+paddingTop : _0xf63f[7],
+borderRadius : _0xf63f[8]
+});
+$(_0xf63f[13])[_0xf63f[9]](_0xf63f[12], _0xf63f[8]);
+$(_0xf63f[16])[_0xf63f[15]]("" + _0xf63f[14]);
+$(_0xf63f[19])[_0xf63f[18]]($(_0xf63f[17]));
+$(_0xf63f[23])[_0xf63f[22]](_0xf63f[20], _0xf63f[21]);
+$(_0xf63f[0])[_0xf63f[27]](_0xf63f[26])[_0xf63f[9]](_0xf63f[24], _0xf63f[25]);
+$(_0xf63f[0])[_0xf63f[27]](_0xf63f[26])[_0xf63f[9]](_0xf63f[24], _0xf63f[25]);
+$("" + _0xf63f[29])[_0xf63f[1]](_0xf63f[28]);
+$(_0xf63f[31])[_0xf63f[9]](_0xf63f[12], _0xf63f[30]);
+$(_0xf63f[32])[_0xf63f[9]](_0xf63f[12], _0xf63f[8]);
+$(_0xf63f[33])[_0xf63f[9]](_0xf63f[12], _0xf63f[8]);
+$(_0xf63f[0])[_0xf63f[27]](_0xf63f[26])[_0xf63f[9]](_0xf63f[24], _0xf63f[25]);
