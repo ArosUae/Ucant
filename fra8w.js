6405d5fc22429d6025c348f8937b7c86e6d13082
ArosUae <65586885+ArosUae@users.noreply.github.com>
2020-06-26 04:26:41 +0400

    Add files via upload

---
+++ fra8w.js
@@ -0,0 +1,111 @@
+$(`<img src="https://www.qwafi1.com/sico/z1d1mmgc5610.gif
+"
+ style="width:100%;color:#F26870;margin-top: 0px;border: 0px solid#F26870;margin-left: 0px;" alt="banner" class="login-banner__img">
+`).insertBefore('.nav-tabs');
+$(`<style>
+.rpwd {width: 60%!important;border-radius: 50px!important;border:2px solid;}
+.uroomz.border {width: fit-content!important;border-radius: 50px!important;margin-top: -46px!important;margin-left: 200px!important;}
+.roomz.selbox.form-control {width: 100%!important;border:2px solid;border-radius: 50px!important;}
+.upower.border {width: fit-content!important;border-radius: 50px!important;margin-top: -47px!important;margin-left: 190px!important;}
+.userpower.selbox.form-control {width: 100%!important;border:2px solid;border-radius: 50px!important;}
+.userdays {width: 56%!important;border-radius: 50px!important;border:2px solid;}
+.ulikec.border {width: fit-content!important;border-radius: 50px!important;border:2px solid;}
+.likec {width: 56%!important;border-radius: 50px!important;border:2px solid;}
+.ae.fa.chats.label.fa-comment.label-primary {width:46px!important;border-radius: 0px 0px 0px 0px!important}
+.ae.fa.label.label-primary.fa-user {width:46px!important;border-radius: 30px 0px 0px 30px!important;}
+.ae.fa.label.label-primary.fa-users {width: 85px!important;border-radius: 0px 0px 0px 0px!important;margin-left: 0px!important;}
+.ae.fa.label.label-primary.fa-commenting-o {width: 85px!important;border-radius: 0px 0px 0px 0px!important;margin-left: 0px!important;}
+.ae.fa.label.fa-users.btnClAlGr {width:25px!important;border-radius: 0px 30px 30px 0px!important;margin-left: 0px!important;}
+.ae.label.label-primary.fa.fa-gear {width: 30px!important;border-radius: 0px 0px 0px 0px!important;}
+</style>`).insertBefore('body');
+
+$('.uzr.fl.corner.borderg').css({'margin': '0px 0',borderRight: '0px solid #ffffff',borderLeft: '0px solid #ffffff',borderTop: '.0px solid #ffffff',borderBottom: '.0px solid #ffffff',paddingTop: '3px',borderRadius: '0px 0px 0px 0px'});
+$('.nav-tabs').addClass('fl').css('width','100%');
+$(`<style>
+}
+span.s1.fa.fa-user.label.badgex.label-as-badge.label-success
+	{
+	padding:0px 3px 2px;
+	margin-right:5px
+}
+.fr.borderg,.fr.borderg a
+	{
+	border-radius:3px!important;
+	margin-right:0px!important;
+	border:black solid 0px;
+	color:00000;
+        
+}
+ul.nav.nav-tabs>li>a
+	{
+	color: 787878;
+	border:none;
+	text-align:center;
+	border-radius:3px;
+	display:initial;
+	width:80%;
+	float:left;
+	padding:0!important;
+        Background-color: 787878;
+}
+ul.nav.nav-tabs>li.active>a
+	{
+	border:none;
+	border-radius:3px
+}
+ul.nav.nav-tabs>li
+	{
+	width:32%;
+	margin:0 5px 0 0
+}
+ul.nav.nav-tabs>li>.fa-user-plus:before,ul.nav.nav-tabs>li>.fa-user:before,ul.nav.nav-tabs>li>.fa-user:before
+	{
+	float:right;
+	margin-right:-26%;
+	border:1px solid;
+	padding:0 2px 1px;
+	border-radius:3px;
+	width:23px;
+	text-align:center;
+	color:787878
+}
+ul.nav.nav-tabs
+	{
+	border-top:1px solid#190604;
+	margin-top:1px;
+	float:right;
+	width:100%;
+	padding:3px 1px
+}
+input#u1,input#u2,input#pass1,input#u3,input#pass2
+	{
+	width:50%;
+	height:31px;
+	margin-left:40.5px;
+	margin-bottom:5.5px;
+	border:1px solid#EDEDED;
+	border-radius:3px;
+	text-align:center
+}
+div#asim13
+	{
+	width:49%;
+	top:122px;
+	position:absolute;
+	left:3px
+}
+div#l1
+	{
+	padding:16.8px!important
+}
+div#d2bc {width:312px!important;}
+div.dpnl.bg {width:312px!important;}
+.well {font-family: 'jazeera',FontAwesome;font-size: 16px!important;padding: 15px!important;text-align: center!important;width: 101%;color: #;white-space: nowrap;border-radius: 14px 14px 14px 14px;}
+.border {shadow:1px 1px 1px 1px ridge rgb(137, 137, 137); box-shadow:1px 1px 1px 1px rgb(137, 137, 137) }
+.fitimg.fl.u-pic  {box-shadow: 0 1px 1px -1px rgb(137, 137, 137), 0 0 5px rgb(137, 137, 137);border-radius: 5px 5px 5px 5px;;}
+.border.corner, .border.corner {box-shadow: inset 0 0px 0px rgb(137, 137, 137), 0 0 4px rgb(137, 137, 137);border: 2px solid #ffffff;as:13px;}
+</style>`).insertBefore("body");
+$('.checkbox label').html(`<input id="stealth" type="checkbox" value="" maxlength="10"><i class="fa fa-eye-slash" aria-hidden="true">`)
+$(`<font size="4" color="#0000"><marquee direction="right" width="100%" height="20" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5" style="height: 20px; width: 100%;background-color:625D63;"> زورنا الكـرم إهلا وسهلآ‘ بكم في دردشـة [ شــــــات قــوافي كــنــق شـاتات قوافي ] ، نتمنآ لكم قضاء اجمل الاوٍقآت معنآ ، ـآ‘لآ‘دإْرٍه .</marquee></font>`).insertBefore('.nav-tabs');
+$(`<center><div style="!important; "  id="ye" margin: 2px;" class="fa fa-heartbeat label label-primary" type="button" data-toggle="collapse" data-target="#demo" aria-expanded="false" aria-controls="collapseExample">&nbsp; ● ·(  قــوآفــي )· ● &nbsp;</div><div class="collapse" id="demo"><center><div class="well"><p lass="bg-primary"></i></p>تصــميــم : فراقك خطه من ايران </p><iframe width="100%" height="664" scrolling="no" frameborder="no" allow="autoplay" src=""></iframe>`).insertBefore('#d2');
+$('.u-msg.dots').removeClass('dots');
