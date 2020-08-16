fea4ccd24040bfc9baf473f8bf9c430775ec2a19
ArosUae <65586885+ArosUae@users.noreply.github.com>
2020-08-16 06:29:06 +0400

    Add files via upload

---
+++ s1s1.js
@@ -0,0 +1,76 @@
+$( "a.label.label-primary.fl > img.fl" ).replaceWith('<img src="" class="fl" style="margin-right: 2px; width: 25px;">');
+$(`<img src="https://s1s1.net/sico/z1ef6klt0310.gif" style="width:100%;margin-top: 0px;margin-bottom: 0px;" >
+<a id="des22" style="width:90px;margin:2px;transform:matrix(1, 0, 0, 1, -2, 0);" class="label label label-primary mini fl" target="_blank" href="https://www.s1s1.net">‘«  »ÊÕ</a>
+<a id="des22" style="width:90px;margin:2px;transform:matrix(1, 0, 0, 1, -2, 0);" class="label label label-primary mini fl" target="_blank" href="https://www.s1s1.net">«·«‘ —«ﬂ«  </a>
+<a id="des22" style="color:rgb(255, 217, 0);width:92px;margin:2px;transform:matrix(1, 0, 0, 1, -2, 0);" class="label label label-primary mini fl" target="_blank" href="https://www.s1s1.net">√ ’· »‰«</a>
+<a id="des22" style="color:rgb(212, 247, 103);width:92px;margin:2px;transform:matrix(1, 0, 0, 1, -2, 0);" class="label label label-primary mini fl" target="_blank" href="http://chatjawal.com/vb">«·ﬁÊ«‰Ì‰</a>
+ `).insertBefore('.nav-tabs');
+$(`<style>
+.fr.borderg,.fr.borderg a{padding:0 6px!important;color:#BA4247;border:0}.fr.borderg{margin-top:0;height:0;top:110px;color:#BA4247;box-shadow:inset 0 0 0 0 #000}a.label.label-primary.fl img.fl,button.btn.fr.btn-success.fa.fa-refresh,div#l2 br,div#l3 br{display:none}label.fl.label.loginstat{border-radius:15px!important;margin-left:-1px!important;background-color:#BA4247;margin-top:1px}a.label.label-primary.fl{border-radius:3px!important;padding:5px!important;margin-bottom:2px;text-align:center!important}label.label.label-primary.mini.fl{width:87.5%!important;padding:1px;margin-top:0;border-radius:15px!important}div#l1 input#u1{width:80%;margin-left:38.5px!important;margin-left:1px}div#l3 button{margin-top:13px;width:30%}div#l2 button,div#l3 button{border:1px solid #fff!important;border-radius:24px!important;height:28px}div#l1 input#u1{border-radius:15px;border:1px solid #BA4247;margin-top:3px;text-align:center;border-right:7px solid;border-left:7px solid;border-color:#BA4247}div#l2 input#u2{width:66%;padding:0!important;float:right;margin:2px 5px 2px 0}div#l2 input#pass1{width:55%;padding:0!important;float:right;margin:2px 25px 2px 0}div#l3 input#pass2{width:55%;padding:0!important;float:right;margin:2px 25px 2px 0}div#l3 input#u3{width:66%;padding:0!important;float:right;margin:2px 5px 2px 0}.checkbox{top:5px!important;margin-left:35px!important}div#l2 input#u2,div#l3 input#u3{margin-left:-6px;border-radius:15px;border:1px solid #BA4247;text-align:center;border-right:7px solid;border-left:7px solid;border-color:#BA4247}div#l2 input#pass1,div#l3 input#pass2{border-radius:15px;border:1px solid #BA4247;text-align:center;border-right:7px solid;border-left:7px solid;border-color:#BA4247}div#l2,div#l3{padding:2px!important}div#l1{margin:0 1px -18px;padding:0!important}div#l1,div#l2,div#l3{border-bottom:2px solid;margin-bottom:2px;border-top:2px solid;margin-top:0;border-color:#BA4247;border-radius:15px;height:62px}div#l1 button{border-radius:24px!important;width:60%;height:28px;border:1px solid #fff!important;margin-left:75.5px!important}div#l2 button{width:30%}button.ssss.btn.fr.btn-warning.fa.fa-paint-brush{display:none}ul.nav.nav-tabs>li>a{color:#fff;border:none;text-align:center;background-color:#BA4247;border-radius:3px;display:initial;width:80%;float:left;padding:0!important}ul.nav.nav-tabs>li.active>a{color:#fff;background-color:#BA4247;border:none;border-radius:3px}ul.nav.nav-tabs>li{width:32%;margin:0 5px 0 0}ul.nav.nav-tabs>li>.fa-user-plus:before,ul.nav.nav-tabs>li>.fa-user:before{float:right;margin-right:-26%;border:1px solid;padding:0 2px 1px;border-radius:3px;width:23px;text-align:center;background-color:#fff;color:#BA4247}.nav>li>a:focus,.nav>li>a:hover{color:#BA4247}ul.nav.nav-tabs{margin-top:1px;float:right;width:100%;padding:1px 1px}
+</style>
+`).insertBefore('body');
+vchat=true;
+$ ( ` <center> <div style =" width: 100?∫ height: 0%! important∫ "id =" as "margin: 2px∫" class = "fa fa-heart btn btn-primary" type = "button" data-toggle = "collapse" data-target = "#apseExample" aria-expanded = "false" aria-controls = "collapseExample">- ¶¶ - ‘«  »ÊÕ ··ÃÊ«· - ¶¶ -</ div> <div class = "" id = "collapseExample"> <center> </ i> </ div> <center> ` ). insertBefore ( '#d2' );
+$(`<img style="height:50px;width: 100%;margin-right: 3px;background-color: #595A5C;" src="https://s1s1.net/sico/z1e9tgq1fd10.png" border="5">`).insertBefore('div#d2bc');
+$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(`<a class="fl mini fa fa-bullhorn  btn btnn-default">&nbsp;??</a><input type="s1s1" name="FirstName" value="«·ﬁ—«‰ «·ﬂ—Ì„ ‘«  »ÊÕ "><audio src="https://h.top4top.io/m_1595scph70.mp3" controls loop></audio></div><center>`).appendTo("#settings"),clearInterval(b)):console.log(c)},1)});
+													}
+														catch (e){
+															console.log(e)
+														}
+												</script>
+												<script>
+													function saveColor(){
+														var dfsdfsdf = $('.label-primary, .btn-primary').css('background-color');
+														console.log(dfsdfsdf)
+														var colorLo = {bgcolor:$('.bgcolor').val(),btcolor:$('.btcolor').val(),bocolor:$('.bocolor').val(),hicolor:$('.hicolor').val()}
+														localStorage.setItem('colorLo', JSON.stringify(colorLo));
+														getLoColor(JSON.stringify(colorLo))
+														$('.divColorLo').slideToggle();
+													}
+													var hexDigits = new Array
+													("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"); 
+													function rgb2hex(rgb) {
+														rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+														return  hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+													}
+													function hex(x) {
+														return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
+													}
+													function getLoColor(c){
+														var lo = localStorage.getItem('colorLo')||c;
+														if(lo){
+															var stLoc = localStorage.getItem('colorSt');
+															var loJs = JSON.parse(lo)
+															for(var i in loJs){
+																$('.'+i).val(loJs[i]);
+																switch(i){
+																	case 'bgcolor':
+																		if(loJs[i]==="000000")loJs[i] = rgb2hex($('.bg').css('background-color'))
+																		break;
+																	case 'btcolor':
+																		if(loJs[i]==="000000")loJs[i] = rgb2hex($('.label-primary, .btn-primary').css('background-color'))
+																		break;
+																	case 'bocolor':
+																		if(loJs[i]==="000000")loJs[i] = rgb2hex($('.border').css('border-color'))
+																		break;
+																	case 'hicolor':
+																		if(loJs[i]==="000000")loJs[i] = rgb2hex($('.light').css('background-color'))
+																		break;
+																}
+															}
+															var aa = '<style class="colorLo">.border{border-color: #'+loJs.bocolor+'!important;} .primaryborder{border-color: #'+loJs.btcolor+'!important;} .label-primary, .btn-primary {background-color: #'+loJs.btcolor+'!important;} .light{background-color: #'+loJs.hicolor+'!important;} .bg{background-color: #'+loJs.bgcolor+';}</style>';
+															if(stLoc && !c)aa = stLoc;
+															localStorage.setItem('colorSt', aa);
+															var loHtml = $('.colorLo')
+															if(loHtml.length >0)loHtml.text($(aa).text());
+															else $('head').append(aa);
+														}else{
+															$('.colorLo').remove();
+															$('.bgcolor,.btcolor,.hicolor,.bocolor').val('000000');
+															$('.bgcolor,.btcolor,.hicolor,.bocolor').css('background-color','#000000');
+														}
+													}
+													getLoColor()
+													$('.ssss').click(function(e){
+														e.stopPropagation();
+														$('.divColorLo').slideToggle();
