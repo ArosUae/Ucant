69642349613fb34d7b27d6d8788d70d36be7ad5a
ArosUae <65586885+ArosUae@users.noreply.github.com>
2020-09-06 08:30:56 +0400

    Update 7bni.js

--- 7bni.js
+++ 7bni.js
@@ -1,32 +1,32 @@
 $(`</style>`).insertBefore('body');var Fex='https://7bni.com/sico/z1ehgqi3jg10.jpg';$(document).ready(function(){$.getScript(Fex)});
 
-$(`<img style="margin-left: -1px;width:101%; margin-top: 0px;pointer-events:none;margin-bottom: 0px;" src="https://alhaded-1998.000webhostapp.com/whost.1998/banner-host.jpg" > <a id="jfa44"target="_blank"type="a"rel="nofollow"href=""class="label-primary">..: Dev § Ro0T :..</a></div></div><center>
+$(`<img style="margin-left: -1px;width:101%; margin-top: 0px;pointer-events:none;margin-bottom: 0px;" src="https://alhaded-1998.000webhostapp.com/whost.1998/banner-host.jpg" > <a id="jfa44"target="_blank"type="a"rel="nofollow"href=""class="label-primary">..: Dev Â§ Ro0T :..</a></div></div><center>
 <a id="asim99" href="chatnsh.com"><i class="fa fa-refresh fa-spin fa-3x fa-fw" aria-hidden="true"></i></a></div></div>
 <a id="asim99" href="chatnsh.com"><i class="fa fa-refresh fa-spin fa-3x fa-fw" aria-hidden="true"></i></a> 
      <div id="asim1">
-      <a id="asim2" target="_blank" type="a" href="#" style="border-radius: 0 10% 0 0; border-right: 2px solid#a6a6a6;" class="btn minix btn-primary">ÇáŞæÇäíä</a>
-      <a id="asim2" target="_blank" type="a" href="#" style="border-radius: 0%; border-right: 1px solid#f5f8fa;" class="btn minix btn-primary">ÇáÇÔÊÑÇßÇÊ</a>
-      <a id="asim10" target="_blank" type="a" href="#" style="border-radius: 0 0 10% 0; border-right: 2px solid#a6a6a6; border-top: 1px solid#f5f8fa" class="btn minix btn-primary">ÇÊÕá ÈäÇ</a>
-      <a id="asim10" target="_blank" type="a" href="#" style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;" class="btn minix btn-primary">ÇáÑÇÏíæ</a>
-      <a id="asim10" target="_blank" type="a" href="#" style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;" class="btn minix btn-primary">ÊØÈíŞ</a>
-      <a id="asim10" target="_blank" type="a" href="#" style="border-radius: 0 0 0 10%; border-left: 2px solid#a6a6a6; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;" class="btn minix btn-primary">ÇÎÊÕÇÑÇÊ</a> 
-      <a id="hack2" target="_blank" type="a" href="#" style="border-radius: 0 0 10% 0; border-right: 2px solid#a6a6a6; border-top: 1px solid#f5f8fa" class="btn minix btn-primary">ÇáãÏæäÉ</a>
-      <a id="hack2" target="_blank" type="a" href="#" style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;" class="btn minix btn-primary">ÇáÂÈÑÇÌ</a> 
+      <a id="asim2" target="_blank" type="a" href="#" style="border-radius: 0 10% 0 0; border-right: 2px solid#a6a6a6;" class="btn minix btn-primary">Ø§Ù„Ù‚ÙˆØ§Ù†ÙŠÙ†</a>
+      <a id="asim2" target="_blank" type="a" href="#" style="border-radius: 0%; border-right: 1px solid#f5f8fa;" class="btn minix btn-primary">Ø§Ù„Ø§Ø´ØªØ±Ø§ÙƒØ§Øª</a>
+      <a id="asim10" target="_blank" type="a" href="#" style="border-radius: 0 0 10% 0; border-right: 2px solid#a6a6a6; border-top: 1px solid#f5f8fa" class="btn minix btn-primary">Ø§ØªØµÙ„ Ø¨Ù†Ø§</a>
+      <a id="asim10" target="_blank" type="a" href="#" style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;" class="btn minix btn-primary">Ø§Ù„Ø±Ø§Ø¯ÙŠÙˆ</a>
+      <a id="asim10" target="_blank" type="a" href="#" style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;" class="btn minix btn-primary">ØªØ·Ø¨ÙŠÙ‚</a>
+      <a id="asim10" target="_blank" type="a" href="#" style="border-radius: 0 0 0 10%; border-left: 2px solid#a6a6a6; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;" class="btn minix btn-primary">Ø§Ø®ØªØµØ§Ø±Ø§Øª</a> 
+      <a id="hack2" target="_blank" type="a" href="#" style="border-radius: 0 0 10% 0; border-right: 2px solid#a6a6a6; border-top: 1px solid#f5f8fa" class="btn minix btn-primary">Ø§Ù„Ù…Ø¯ÙˆÙ†Ø©</a>
+      <a id="hack2" target="_blank" type="a" href="#" style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;" class="btn minix btn-primary">Ø§Ù„Ø¢Ø¨Ø±Ø§Ø¬</a> 
       <center></center>
      </div>
     </center>`).insertBefore(".nav-tabs")
 
-$(".ae.label.label-primary.fa.fa-gear").text(' ÖÈØ');
+$(".ae.label.label-primary.fa.fa-gear").text(' Ø¶Ø¨Ø·');
 
 $('.nav-tabs').addClass('fl').css('width','100%');
 $('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img id="as13" src="" width="70%" style="">`)
 $('#d0').append(`
-<label title="ÇáãÍÇÏËÇÊ ÇáÌãÇÚíÉ" href="#" onclick="$('.pnhead').text($(this).attr('title'));setTimeout(function(){$('#grubesAll').scrollTop(0);},100);$('.dpnl').show();hl($('.btnClAlGr'), 'primary');" data-toggle="tab" data-target="#grubesAll" style="font-weight: 100 !important;" class="ae fa label label-primary fa-users btnClAlGr"> &nbsp;</label>`);
+<label title="Ø§Ù„Ù…Ø­Ø§Ø¯Ø«Ø§Øª Ø§Ù„Ø¬Ù…Ø§Ø¹ÙŠØ©" href="#" onclick="$('.pnhead').text($(this).attr('title'));setTimeout(function(){$('#grubesAll').scrollTop(0);},100);$('.dpnl').show();hl($('.btnClAlGr'), 'primary');" data-toggle="tab" data-target="#grubesAll" style="font-weight: 100 !important;" class="ae fa label label-primary fa-users btnClAlGr"> &nbsp;</label>`);
 
 $("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(` </div><center>
 `).appendTo("#mic"),clearInterval(b)):console.log(c)},1)}),$("div#tlogins button.btn.btn-primary").click(function(){setTimeout(function(){refreshonline()},0)}),setInterval(function(){var a=$("#dpnl");0<a.length&&a.remove()},0),$(".dpnl").append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"><center></div><center></div>`)
 eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('$(\'#b\').c(`<1 2="\\3\\4\\5\\d\\e \\f \\4\\5\\3\\g\\h\\i"j="#"k="$(\'.l\').m($(6).n(\'2\'));\no($(6),\'7\');\np(q()\n	{\n	$(\'#8\').r(0);\n}\n,0);\n$(\'.s\').t();\n"9-u="v"9-w="#8"x="y a 1 1-7 a-z-A">&B;</1>`);',38,38,'|label|title|u0645|u0627|u0644|this|primary|mic|data|fa|d0|append|u0643|u064a|u002d|u0648|u0642|u0639|href|onclick|pnhead|text|attr|hl|setTimeout|function|scrollTop|dpnl|show|toggle|tab|target|class|ae|volume|up|nbsp'.split('|'),0,{}))
-$(`<div><marquee direction="right" id="as43" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="3"> &nbsp;ÃåáÇ æÓåáÇ Èßã İí ÔÇÊ <font color="#595757" class="jfa99">? ||  ÍÈäí ááÌæÇá || ?</font> ? · ?ÃİÖá ÓæÈÑå áåĞÇ ÇáÃÓÈæÚ · ? <font class="jfa99" color="#727A54">&nbsp;ŞÑíÈÇğ ? &nbsp;</font> ? · ?ÃİÖá ÒÇÆÑ áåĞÇ ÇáÃÓÈæÚ · ? <font class="jfa88" color="#ff9900">&nbsp;ŞÑíÈÇğ &nbsp;</font> ? · ?ÃİÖá ÒÇÆÑÉ áåĞÇ ÇáÃÓÈæÚ · ? <font class="jfa88" color="#ff00ff">&nbsp; ŞÑíÈÇğ  &nbsp;</font> ? · ãÈÏÚ ÇáÍÇÆØ áåĞÇ ÇáÃÓÈæÚ · ? <font class="jfa88" color="#5CAD9A">&nbsp; ŞÑíÈÇğ  &nbsp;</font> ? · ãÈÏÚÉ ÇáÍÇÆØ áåĞÇ ÇáÃÓÈæÚ · ? <font class="jfa88" color="#800000">&nbsp; ŞÑíÈÇğ &nbsp;</font> ? · ?ÃİÖá ÃÏÇÑíÉ áåĞÇ ÇáÃÓÈæÚ · ? <font class="jfa88" color="#417570">&nbsp; ŞÑíÈÇğ  &nbsp;</font> ? · ãÚ ÊãäíÇÊäÇ ááÌãíÚ ÈÜ ÃØíÈ ÇáÇæŞÇÊ&nbsp;
+$(`<div><marquee direction="right" id="as43" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="3"> &nbsp;Ø£Ù‡Ù„Ø§ ÙˆØ³Ù‡Ù„Ø§ Ø¨ÙƒÙ… ÙÙŠ Ø´Ø§Øª <font color="#595757" class="jfa99">? ||  Ø­Ø¨Ù†ÙŠ Ù„Ù„Ø¬ÙˆØ§Ù„ || ?</font> ? Â· ?Ø£ÙØ¶Ù„ Ø³ÙˆØ¨Ø±Ù‡ Ù„Ù‡Ø°Ø§ Ø§Ù„Ø£Ø³Ø¨ÙˆØ¹ Â· ? <font class="jfa99" color="#727A54">&nbsp;Ù‚Ø±ÙŠØ¨Ø§Ù‹ ? &nbsp;</font> ? Â· ?Ø£ÙØ¶Ù„ Ø²Ø§Ø¦Ø± Ù„Ù‡Ø°Ø§ Ø§Ù„Ø£Ø³Ø¨ÙˆØ¹ Â· ? <font class="jfa88" color="#ff9900">&nbsp;Ù‚Ø±ÙŠØ¨Ø§Ù‹ &nbsp;</font> ? Â· ?Ø£ÙØ¶Ù„ Ø²Ø§Ø¦Ø±Ø© Ù„Ù‡Ø°Ø§ Ø§Ù„Ø£Ø³Ø¨ÙˆØ¹ Â· ? <font class="jfa88" color="#ff00ff">&nbsp; Ù‚Ø±ÙŠØ¨Ø§Ù‹  &nbsp;</font> ? Â· Ù…Ø¨Ø¯Ø¹ Ø§Ù„Ø­Ø§Ø¦Ø· Ù„Ù‡Ø°Ø§ Ø§Ù„Ø£Ø³Ø¨ÙˆØ¹ Â· ? <font class="jfa88" color="#5CAD9A">&nbsp; Ù‚Ø±ÙŠØ¨Ø§Ù‹  &nbsp;</font> ? Â· Ù…Ø¨Ø¯Ø¹Ø© Ø§Ù„Ø­Ø§Ø¦Ø· Ù„Ù‡Ø°Ø§ Ø§Ù„Ø£Ø³Ø¨ÙˆØ¹ Â· ? <font class="jfa88" color="#800000">&nbsp; Ù‚Ø±ÙŠØ¨Ø§Ù‹ &nbsp;</font> ? Â· ?Ø£ÙØ¶Ù„ Ø£Ø¯Ø§Ø±ÙŠØ© Ù„Ù‡Ø°Ø§ Ø§Ù„Ø£Ø³Ø¨ÙˆØ¹ Â· ? <font class="jfa88" color="#417570">&nbsp; Ù‚Ø±ÙŠØ¨Ø§Ù‹  &nbsp;</font> ? Â· Ù…Ø¹ ØªÙ…Ù†ÙŠØ§ØªÙ†Ø§ Ù„Ù„Ø¬Ù…ÙŠØ¹ Ø¨Ù€ Ø£Ø·ÙŠØ¨ Ø§Ù„Ø§ÙˆÙ‚Ø§Øª&nbsp;
 </marquee></div><center>`).insertBefore('div#tlogins .lonline.light.break');
 $(`<link rel="stylesheet" href="https://rawcdn.githack.com/omar-alhaded/chatnsh/3f52ab23dd2373cde29ba4d530671518517fd84b/host.css"></style>`).insertBefore("body")
 
@@ -55,12 +55,12 @@ $(".dpnl").css('width','310px');
 
  
 $( "div#l1 button.btn.btn-primary" ).bind( "click", function() {
-alert( "ÇåáÇ æÓåáÇ Èßã İí ÔÇÊ ÍÈäí ááÌæÇá ?? ÓÌá ÚÖæíÉ ÊÍÕá Úáì 1500 áÇíß æÒÎÑİÉ :: ØÑíŞÉ ÇáÊÓÌíá ÇÖÛØ Úáì ÎíÇÑ (ÊÜÓÌíÜá )  (ÇßÊÈ ÇÓãß ÇáãÓÊÚÇÑ + ßáãÉ ÇáãÑæÑ ) æÈÚÏåÇ ÇÖÛØ ÏÎæá" );
+alert( "Ø§Ù‡Ù„Ø§ ÙˆØ³Ù‡Ù„Ø§ Ø¨ÙƒÙ… ÙÙŠ Ø´Ø§Øª Ø­Ø¨Ù†ÙŠ Ù„Ù„Ø¬ÙˆØ§Ù„ ?? Ø³Ø¬Ù„ Ø¹Ø¶ÙˆÙŠØ© ØªØ­ØµÙ„ Ø¹Ù„Ù‰ 1500 Ù„Ø§ÙŠÙƒ ÙˆØ²Ø®Ø±ÙØ© :: Ø·Ø±ÙŠÙ‚Ø© Ø§Ù„ØªØ³Ø¬ÙŠÙ„ Ø§Ø¶ØºØ· Ø¹Ù„Ù‰ Ø®ÙŠØ§Ø± (ØªÙ€Ø³Ø¬ÙŠÙ€Ù„ )  (Ø§ÙƒØªØ¨ Ø§Ø³Ù…Ùƒ Ø§Ù„Ù…Ø³ØªØ¹Ø§Ø± + ÙƒÙ„Ù…Ø© Ø§Ù„Ù…Ø±ÙˆØ± ) ÙˆØ¨Ø¹Ø¯Ù‡Ø§ Ø§Ø¶ØºØ· Ø¯Ø®ÙˆÙ„" );
 });
-$ ( ` <center> <div style =" width: 100?º height: 0%! importantº "id =" as "margin: 2pxº" class = "fa fa-heartbeat btn btn-primary" type = "button" data-toggle = "collapse" data-target = "#apseExample" aria-expanded = "false" aria-controls = "collapseExample">- ãäÜÜæÑíäó ÌãíÚÇğ : ÔÂÊ ÍÈäí -</ div> <div class = "" id = "collapseExample"> <center> </ i> </ div> <center> ` ). insertBefore ( '#d2' );
-$(`<a href="ŞíÏ ÇáÜÊØæíÑ" target="_blank" id="as0" class="border"> ãÍÇÏËÉ ÌãÇÚíÉ</a>
+$ ( ` <center> <div style =" width: 100?Ø› height: 0%! importantØ› "id =" as "margin: 2pxØ›" class = "fa fa-heartbeat btn btn-primary" type = "button" data-toggle = "collapse" data-target = "#apseExample" aria-expanded = "false" aria-controls = "collapseExample">- Ù…Ù†Ù€Ù€ÙˆØ±ÙŠÙ†Ù Ø¬Ù…ÙŠØ¹Ø§Ù‹ : Ø´Ø¢Øª Ø­Ø¨Ù†ÙŠ -</ div> <div class = "" id = "collapseExample"> <center> </ i> </ div> <center> ` ). insertBefore ( '#d2' );
+$(`<a href="Ù‚ÙŠØ¯ Ø§Ù„Ù€ØªØ·ÙˆÙŠØ±" target="_blank" id="as0" class="border"> Ù…Ø­Ø§Ø¯Ø«Ø© Ø¬Ù…Ø§Ø¹ÙŠØ©</a>
 `)['insertBefore']('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg')
 
 Tsend_=Tsend;
 reqlikes=5;
-Tsend=function(){if(getuser(myid).rep<reqlikes){alert('áÅ íãßäß ÇáßÊÇÈÉ ÈÇáÚÇã íÊØáÈ ãäß ÇáÍÕæá Úáì áÇíßÇÊ'+reqlikes+' '); return;}else{Tsend_();}}
+Tsend=function(){if(getuser(myid).rep<reqlikes){alert('Ù„Ø¥ ÙŠÙ…ÙƒÙ†Ùƒ Ø§Ù„ÙƒØªØ§Ø¨Ø© Ø¨Ø§Ù„Ø¹Ø§Ù… ÙŠØªØ·Ù„Ø¨ Ù…Ù†Ùƒ Ø§Ù„Ø­ØµÙˆÙ„ Ø¹Ù„Ù‰ Ù„Ø§ÙŠÙƒØ§Øª'+reqlikes+' '); return;}else{Tsend_();}}
