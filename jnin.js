99a10bd8330365e12aaf16fbca94997e489f8539
ArosUae <65586885+ArosUae@users.noreply.github.com>
2020-09-07 15:57:08 +0400

    Create jnin.js

---
+++ jnin.js
@@ -0,0 +1,71 @@
+$(`<img src="https://chatcom.live/sico/1599479746302.gif" class="fr" style="width:75%;margin-top: 0px;">
+<a id="des3" href="" style="    text-align: right!important;transform:matrix(1, 0, 0, 1, -3, 0);border-radius: 24px;width:70%;margin:-172px 2px 2px;" class="fl mini  fa   fa-
+ btn btn-primary">شات الخليج | شات العرب | شات جوال</a>
+ <a id="des3" href="" style="transform:matrix(1, 0, 0, 1, -3, 0);border-radius: 24px;width:24%;margin:-138px 2px 2px;" class="fl mini  fa   fa-
+ btn btn-primary">جنون العاشقين</a>
+ <a id="des3" href="https://chatcom.live/%D9%8A%20%D8%B4%D8%A7%D8%AA%20%D8%AC%D9%86%D9%88%D9%86%20%D8%A7%D9%84%D8%B9%D8%A7%D8%B4%D9%82%D9%8A%D9%86%20%D9%86%D8%AA%D9%85%D9%86%D8%A7%20%D9%84%D9%83%20%D9%82%D8%B6%D8%A7%D8%A1%20%D9%88%D9%82%D8%AA%D9%8D%20%D9%85%D9%85%D8%AA%D8%B9%F0%9F%8C%B9" style="transform:matrix(1, 0, 0, 1, -3, 0);border-radius: 24px;width:27%;margin:-104px 2px 2px;" class="fl mini  fa   fa-
+ btn btn-primary">تطبيق</a>
+ <a id="des3" href="https://wa.me/message/KDPCVFFHYEG7A1" style="transform:matrix(1, 0, 0, 1, -3, 0);border-radius: 24px;width:30%;margin:-69px 2px 2px;" class="fl mini  fa   fa-
+ btn btn-primary">واتساب</a>
+ <a id="des3" href="https://chatcom.live/rules.html" style="transform:matrix(1, 0, 0, 1, -3, 0);border-radius: 24px;width:33%;margin:-34px 2px 2px;" class="fl mini  fa   fa-
+ btn btn-primary">القوانين</a>
+
+
+`).insertBefore('.nav-tabs');
+
+$('.hand.nosel.fl.uzr.border').css({'margin': '1px 0',borderRight: '1px solid #1a90a5',borderLeft: '1px solid #1a90a5',borderTop: '1px solid #4F565E',borderBottom: '1px solid #4F565E',paddingTop: '2px',borderRadius: '5px'});
+$('.uzr.fl.corner.borderg').css({'margin': '0px 0',borderRight: '1px solid #1a90a5',borderLeft: '1px solid #1a90a5',borderTop: '.40px solid #a3a9b0',borderBottom: '.50px solid #a3a9b0',paddingTop: '2px',borderRadius: '5px'});  
+$('.room.borderg.hand.nosel.fl').css({'margin': '1px 0',borderRight: '1px solid #1a90a5',borderLeft: '1px solid #1a90a5',borderTop: '1px solid #4F565E',borderBottom: '1px solid #4F565E',paddingTop: '2px',borderRadius: '5px'});
+$(".corner.fa.fa-user.label.label-primary.fr.uc").css("border-radius","5px")
+
+$("[data-target=#wall]").attr('title','   مســـاحه حــره لأبــدآعاتكــم            ')
+
+$('.nav-tabs').addClass('fl').css('width','100%');
+$('.nav-tabs').addClass('fl').css('width','100%');
+$(`<link href="" rel="stylesheet">
+<link rel="stylesheet" href="https://rawcdn.githack.com/omar-alhaded/msj/a8a739b34765ff4339d3d49b61e40ef8c41d6c61/g.css">
+<style>
+.fr.borderg, .fr.borderg a {
+    font-size: 10px!important;
+    padding: 0 5px!important;
+}
+.fr.borderg, .fr.borderg a {
+    border-radius: 3px!important;
+    background-color: #1c91a7!important;
+    background-image: linear-gradient(90deg,#61adcc 0,#3390b3 10%,#1e6a87 51%,#3390b3 89%,#61adcc 100%);
+    border: black solid 0px;
+    color: #fff;
+}
+.fr.borderg, .fr.borderg a {
+    margin-top: 5px;
+}
+label.fl.label.loginstat, ul.nav.nav-tabs.fl li a {
+    font-family: el messiri,FontAwesome!important;
+	color: #ffca00!important;
+}
+.hmsg {background-color: rgb(235, 241, 255)!important;}
+a.label.label-primary.fl {display: none}
+#des1 {margin-bottom: 2px;font-family: 'El Messiri',FontAwesome!important;background-image: linear-gradient(90deg,#61adcc 0,#3390b3 10%,#1e6a87 51%,#3390b3 89%,#61adcc 100%);color: #f9f9f9;border-radius: 3px;padding: 4px 0!important;padding-bottom: 3px;}
+
+</style>`).insertBefore('body');
+$(".u-pic").css("border-radius","3px");
+$( "button.btn.btn-primary" ).css( "border-radius", "3px" )
+$(".label-primary").css("border-radius","3px")
+$('.nav-tabs').addClass('fl').css('width','100%');
+Tsend_=Tsend;
+reqlikes=10;
+Tsend=function(){if(getuser(myid).rep<reqlikes){alert('لإ يمكنك الكتابة بالعام يتطلب منك الحصول على لايكات'+reqlikes+' '); return;}else{Tsend_();}}
+
+sendpic_=sendpic;
+reqlikes=10;
+sendpic=function(){if(getuser(myid).rep<reqlikes){alert('لإ يمكنك تغير صورتك يتطلب منك الحصول على لايكات '+reqlikes+''); return;}else{sendpic_();}}
+
+setprofile_=setprofile;
+reqlikes=10;
+setprofile=function(){if(getuser(myid).rep<reqlikes){alert('لا يمكنك حفظ الأعدادات يتطلب منك الحصول على لايكات '+reqlikes+''); return;}else{setprofile_();}}
+$("div#tlogins input").attr("maxlength",13),$("textarea#tbox.fl.corner.tbox.filw").attr("maxlength",1000);
+$(".ubnr").remove(); 
+$(".fr.co").hide();
+$('.uzr.fl.corner.borderg').css({'margin': '0px 0',borderRight: '0px solid #ffffff',borderLeft: '0px solid #ffffff',borderTop: '.0px solid #ffffff',borderBottom: '.0px solid #ffffff',paddingTop: '3px',borderRadius: '0px 0px 0px 0px'});
+
+$('.u-msg.dots').removeClass('dots');
