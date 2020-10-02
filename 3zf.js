72a94048aad88e640bb614676e29a2cf0b353c18
ArosUae <65586885+ArosUae@users.noreply.github.com>
2020-10-02 17:20:22 +0400

    Update 3zf.js

--- 3zf.js
+++ 3zf.js
@@ -1,5 +1,4 @@
-$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?( $(`
-            <div class="hand nosel fl uzr borderg uid174141d0d3bxac536973-xm93uin inroom" style="text-align:left;background-color:white;border-radius:0px!important;width:99%;margin:0px;padding:1px;" v="1000000">
+<div class="hand nosel fl uzr borderg uid174141d0d3bxac536973-xm93uin inroom" style="text-align:left;background-color:white;border-radius:0px!important;width:99%;margin:0px;padding:1px;" v="1000000">
       <img class="fl ustat" style="width:3px;height:36px;margin-left: 1px;" src="imgs/s0.png?2">
       <img style="width: 36px; height: 36px; margin-left: 1px; background-image: url(&quot;https://b.top4top.io/p_1736mc3ai1.jpg&quot;);" class="fitimg fl u-pic ">
       <label class="fl muted fa" style="color:indianred;">&nbsp;</label>
