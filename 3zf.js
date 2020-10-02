9ca0bd7da1dc3407fd7ca979f889457318a87068
ArosUae <65586885+ArosUae@users.noreply.github.com>
2020-10-02 17:16:26 +0400

    Create 3zf.js

---
+++ 3zf.js
@@ -0,0 +1,12 @@
+$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?( $(`
+            <div class="hand nosel fl uzr borderg uid174141d0d3bxac536973-xm93uin inroom" style="text-align:left;background-color:white;border-radius:0px!important;width:99%;margin:0px;padding:1px;" v="1000000">
+      <img class="fl ustat" style="width:3px;height:36px;margin-left: 1px;" src="imgs/s0.png?2">
+      <img style="width: 36px; height: 36px; margin-left: 1px; background-image: url(&quot;https://b.top4top.io/p_1736mc3ai1.jpg&quot;);" class="fitimg fl u-pic ">
+      <label class="fl muted fa" style="color:indianred;">&nbsp;</label>
+      <img class="fr co" style="width:16px;border-radius:1px;" src="flag/kw.gif">
+      <span class="fr uhash" style="color:grey;font-size:70%!important;margin-top: -3px;">#68</span>
+      <div style="width:68%;" class="fl">
+        <div style="width:100%;margin-top:-2px;display: flex;" class="fl">
+          <img class="fl u-ico" style="object-fit: contain;min-height: 14px;">
+          <div class="fl" style="width:82%;"><span style="margin-top: 1px; padding: 0px 8px; max-width: 100%; border-radius: 3px; background-color: rgb(255, 255, 255); color: rgb(111, 17, 110);" class="corner u-topic dots">            ᓚᓅjـــ‏‏‏‏‏‏‏‏‏‏‏‏ــ‏‏‏‏‏‏‏‏‏‏‏ـ‏‏‏‏‏‏‏‏‏‏‏‏ـ‏‏‏‏‏‏‏‏‏‏‏‏c</span></div>
+        </div>
