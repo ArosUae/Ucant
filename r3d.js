8dc9f3d0c1afe5800417c9d289a54e1fa109dacc
ArosUae <65586885+ArosUae@users.noreply.github.com>
2020-10-05 14:28:06 +0400

    Update r3d.js

--- r3d.js
+++ r3d.js
@@ -1,1 +1,20 @@
 $(`<marquee direction="right" width="65%" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif; height: 20px;" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5"><font style="background-color: #‏B7B6BA;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;"> iTa7a.CoM | شات التحلية </marquee></font></b></td>`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success');	
+
+
+
+
+--------------------
+كود الكلمات ، 
+غير الكلمات و الروابط :) 
+و اذا اشترك مع البنر تغير رقم البنر ، و الايدي ، تخليه بدون او تكتب R3D
+$("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'><a href='https://ita7a.com/'>شات كتابي</a></div>").insertAfter($(".loginstat"));  },1000);
+$(`<img src="/sico/1596988518789.gif" style="width:100%;margin-top: 0px;margin-bottom: 0px;" ><center><div id="asim13"><a  
+id="asim21" href="http://dir.ita7a.net/"style="width: 63px;padding: 1px;height: 22px;" class="btn minix btn-primary">دليل شات</a><a id="asim21" href="http://k-7.net/" style="width: 63px;padding: 1px;height: 22px;" class="btn minix btn-primary">شات التحليه</a><a id="asim21" href="http://vb.ita7a.net/"style="width: 55px;padding: 1px;height: 22px;" class="btn minix btn-primary">منتديات</a><a id="asim21" href="http://ta7a.net/"style="width: 63px;padding: 1px;height: 22px;" class="btn minix btn-primary">شات التحليه</a><a id="asim21" href="https://khaleej1chat.com/"style="width: 63px;padding: 1px;height: 22px;" class="btn minix btn-primary">شات التحليه</a><a id="asim21" href="http://jawal-chat.com/"style="width: 63px;padding: 1px;height: 22px;" class="btn minix btn-primary">شات التحليه</a></div></div>
+</div>
+</div>
+<a id="des3" href="https://ita7a.com/chat-name-style.html" style="border-radius: 24px;width:23%;margin:2px;" class="fl mini  fa  fa-graduation-cap btn btn-primary">زخارف</a>
+<a id="des3" style="border-radius: 24px;width:23%;margin:2px;" class="fl mini  fa   fa-star btn btn-default" href="https://ita7a.com/chat-rules.html">القوانين</a>
+<a id="des3" style="border-radius: 24px;width:27%;margin:2px;" class="fl mini  fa fa-graduation-cap btn btn-primary" href="https://ita7a.com/chat-sups.html">الاشتراكات
+</a>
+<div style=" width: 100%; "  id="asim33" class="btn btn-primary btn
+<a href="" target="_blank" id="https://ita7a.com/"><img src="/sico/1592924319238.gif" style="width: 70%;transform:matrix(1, 0, 0, 1, 8, 
