1bd753240526aab37c9e40bd89fff22413242ef8
ArosUae <65586885+ArosUae@users.noreply.github.com>
2020-10-07 20:31:37 +0400

    Create 999.js

---
+++ 999.js
@@ -0,0 +1,22 @@
+  $(`<img src="https://k.top4top.io/p_162489ex10.jpg" class="fr" style="width:100%;margin-right: 2px;">
+<a id="ROOT" href="" style="border-radius: 24px;width:23%;margin:2px;" class="fl mini  fa  fa-graduation-cap btn btn-primary"> الفعاليات</a>
+<a id="ROOT" style="border-radius: 24px;width:23%;margin:2px;" class="fl mini  fa   fa-star btn btn-default" href="">القوانين</a>
+<a id="ROOT" style="border-radius: 24px;width:27%;margin:2px;" class="fl mini  fa fa-graduation-cap btn btn-primary" href="">الإشتركات  
+</a>
+<div style=" width: 100%; " id="ROOT" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"> اداره الموقع </div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary">  <i class="fa fa-paper-plane">  </i> اداره 
+الموقع </p> <i class="fa fa-comment"></i> <i 
+class="fa fa-share"> أمـلَ </i> </p> <i class="fa fa-comment"></i> <i class="fa fa-share">  </i> </p> <i class="fa fa fa-comment"> </i> <i class="fa fa-share"> </i> </div></div><center></div></div><center><center><div><marquee direction="right" width="99%" id="ROOT" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="2" >
+
+`).insertBefore('.nav-tabs');
+
+
+
+
+
+
+
+$ ( ` <center> <div style =" width: 100&#1642;؛ height: 0%! important؛ "id =" as "margin: 2px؛" class = "fa fa-heartbeat btn btn-primary" type = "button" data-toggle = "collapse" data-target = "#apseExample" aria-expanded = "false" aria-controls = "collapseExample"> شات بلاك بيري للجوال </ div> <div class = "" id = "collapseExample"> <center> </ i> </ div> <center> ` ). insertBefore ( '#d2' );
+
+
+
+$(`<marquee direction="right" width="65%" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif; height: 20px;" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5"><font style="background-color: #‏B7B6BA;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;"> https://blby1.com شات بلاك بيري يرحب بكم جميعا ||  </marquee></font></b></td>`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success');
