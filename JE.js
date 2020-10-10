043701d9efc77072371983a4f2f737b47021afad
ArosUae <65586885+ArosUae@users.noreply.github.com>
2020-10-10 19:31:34 +0400

    Add files via upload

---
+++ JE.js
@@ -0,0 +1,152 @@
+<html><head>
+<meta http-equiv="Content-Language" content="fr">
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title> </title>
+<meta name="keywords" content="&#1578;&#1605; &#1575;&#1604;&#1575;&#1582;&#1578;&#1585;&#1575;&#1602; &#1605;&#1606; &#1602;&#1576;&#1604; &#1580;&#1610;&#1601;&#1575;&#1585;&#1575; &#1575;&#1604;&#1588;&#1605;&#1585;&#1610;">
+<style type="text/css">
+.style2 {
+	font-family: "Cooper Black";
+	font-size: x-large;
+	text-shadow: red 0px 0px 4px;
+
+}
+.style3 {
+	color: #FFFF00;
+	text-shadow: red 0px 0px 4px;
+	font-size: 14pt;
+	font-family: "Old English Text MT";
+}
+.style5 {
+	color: #FF0000;
+	text-shadow: red 0px 0px 4px;
+
+}
+.style6 {
+	color: #008000;
+	font-size: 11pt;
+	font-family: "Arial Rounded MT Bold", forte, "comic sans ms", chiller;
+	text-shadow: red 0px 0px 4px;
+
+}
+.style7 {
+	font-size: 13pt;
+	text-shadow: red 0px 0px 4px;
+
+}
+.style8 {
+	color: #008000;
+	text-shadow: red 0px 0px 4px;
+
+}
+.style9 {
+	font-family: "Comic Sans MS";
+	text-shadow: red 0px 0px 4px;
+
+}
+.style10 {
+	font-size: 11pt;
+	text-shadow: red 0px 0px 4px;
+
+}
+.style11 {
+	font-family: "Modern No. 20";
+	font-size: 14pt;
+	text-shadow: red 0px 0px 4px;
+
+}
+.style12 {
+	color: #000000;
+	text-shadow: red 0px 0px 4px;
+
+}
+.style13 {
+	font-family: "Arial Rounded MT Bold", forte, "comic sans ms", chiller;
+	text-shadow: red 0px 0px 4px;
+
+}
+.style14 {
+	font-size: 13pt;
+	font-family: "Arial Rounded MT Bold", forte, "comic sans ms", chiller;
+	text-shadow: red 0px 0px 4px;
+
+}
+.style15 {
+	color: #008000;
+	font-family: "Arial Rounded MT Bold", forte, "comic sans ms", chiller;
+	text-shadow: red 0px 0px 4px;
+
+}
+.style16 {
+	font-size: 11pt;
+	font-family: "MV Boli";
+	color: #FF33CC;
+	text-shadow: red 0px 0px 4px;
+
+}
+.style17 {
+	text-decoration: none;
+	text-shadow: red 0px 0px 4px;
+
+}
+</style>
+<meta name="description" content="&#1578;&#1605; &#1575;&#1604;&#1575;&#1582;&#1578;&#1585;&#1575;&#1602; &#1605;&#1606; &#1602;&#1576;&#1604; &#1580;&#1610;&#1601;&#1575;&#1585;&#1575; &#1575;&#1604;&#1588;&#1605;&#1585;&#1610;">
+</head>
+
+<body style="background-color: rgb(255, 255, 255);">
+
+<p class="style2" align="center"><font face="Tahoma">
+<font size="6" color="#FF0000"><b>#</b></font><b> <span lang="ar-ae">&nbsp; &#1578;&#1605; 
+&#1575;&#1604;&#1585;&#1601;&#1587;&#1614; &#1548; &#1576;&#1606;&#1580;&#1575;&#1581;&#1616; &#1576;&#1585;&#1593;&#1575;&#1610;&#1607;&#1607;&#1614;&nbsp; </span><font size="6" color="#FF0000">#</font></b></font></p>
+<p class="style2" align="center"><span class="style12"><b>
+<font face="Tahoma" size="5"><font color="#FF0000">#</font> <span lang="ar-ae">
+&#1578;&#1605; &#1575;&#1604;&#1575;&#1582;&#1578;&#1585;&#1575;&#1602; &#1605;&#1606; &#1602;&#1576;&#1604; &#1589;&#1601;&#1575;&#1578;&#1610; : &#1605;&#1587;&#1578;&#1585; &#1575;&#1587;&#1591;&#1608;&#1585;&#1607; &#1548; </span><font color="#FF0000">&nbsp;</font>&nbsp;# </font></b></span><font face="Tahoma" size="5"><br>
+&nbsp;</font></p>
+
+<p align="center" class="style2">
+
+<img border="0" src="360215887.png" width="313" height="407"></p>
+
+<font size="7">
+</font><p align="center" class="style2"><font size="7"><b><span class="style12">
+<font size="6" face="Segoe Script" color="#FF0000">./<span lang="en-us">Jevara
+</span></font></span></b></font></p>
+
+
+<p align="center" class="style2"><span class="style12"><span lang="ar-ae"><b>
+<font face="Tahoma" size="5">&#1578;&#1605; &#1580;&#1604;&#1583;&#1603;&#1616; &#1610; &#1605;&#1607;&#1606;&#1583; &#1604;&#1604;&#1605;&#1585;&#1607; &#1575;&#1604;&#1579;&#1575;&#1606;&#1610;&#1600;&#1607; &#1548; &#1593;&#1604;&#1609; &#1575;&#1604;&#1578;&#1608;&#1575;&#1604;&#1610; &#1576;&#1587; 
+&#1607;&#1575;&#1604;&#1605;&#1585;&#1607;&#1614; $ &#1603;&#1575;&#1605;&#1604;&#1616;</font></b></span></span></p>
+<p align="center" class="style2"><span lang="ar-ae"><b>
+<font face="Tahoma" size="5" color="#FF0000">
+<a href=""><font color="#FF0000">
+~~~~~~~~~~~~~</font></a></font></b></span></p>
+<p align="center" class="style2"><span lang="ar-ae"><b>
+<font face="Tahoma" size="5">&#1580;&#1610;&#1601;&#1575;&#1585;&#1575; &#1575;&#1604;&#1588;&#1605;&#1600;&#1585;&#1610;&#1614;</font><font face="Tahoma" size="5" color="#FF0000"> 
+&#1548; &#1605;&#1585; &#1605;&#1606; &#1607;&#1608;&#1606;&#1614; :#</font></b></span></p>
+<p align="center" class="style2"><span lang="en-us"><b>
+<font face="Tahoma" size="5"><a href="">
+<font color="#FF0000">_!_</font></a></font></b></span></p>
+<p align="center" class="style2"><span lang="ar-ae">&#1603;&#1587;&#1605;&#1603; &#1548; &#1593;&#1604;&#1609; &#1603;&#1587; &#1575;&#1605; &#1578;&#1585;&#1576;&#1610;&#1578;&#1603; 
+&#1575;&#1604;&#1608;&#1589;&#1582;&#1600;&#1607; &#1576;&#1587; :$ </span></p>
+<p align="center" class="style2"><span lang="ar-ae">&#1576;&#1585;&#1608;&#1576; &quot;&#1616; </span></p>
+<p align="center" class="style2"><span class="style12">
+<font face="Tahoma" size="4"><font color="#FF0000">G2<b>\</b></font><b> My love 
+: $ - Mr.2stora</b></font></span></p>
+<p align="center" class="style2"><span class="style12"><font size="6"><a href="" target="_blank" class="style17"><span style="background-color: #FFFFFF;</span></font></p>
+<p class=" style2"="" align="center"><font size="5" face="Tahoma"><br>
+</font> </span></a> 
+</font></span><span style="background-color: #FFFFFF;</span></font></p>
+<p class=" style2"="" align="center">
+
+<font size="7">
+<font color="#FF33CC" size="5" face="Tahoma">&nbsp;<embed width="22" height="35" src="https://www.youtube.com/watch?v=GsFlJFt9rRU"></font></font></span></p><font size="7">
+</font>
+
+<font size="5" face="Tahoma">
+</font>
+
+<font size="7" face="AF_Nakesh KurdishMNB">
+
+<font size="5" face="Tahoma">
+ <p></p></font><p>
+
+&nbsp;</p></font></body></html>
