179fac1cf83bbbed80650cf91b7ecf714b73fd62
ArosUae <65586885+ArosUae@users.noreply.github.com>
2020-11-26 12:25:11 +0400

    Create 5o5h.js

---
+++ 5o5h.js
@@ -0,0 +1,3 @@
+<div style=" width: 100%; " id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"> اداره الموقع </div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary">  <i class="fa fa-paper-plane">  </i> اداره 
+الموقع </p> <i class="fa fa-comment"></i> <i 
+class="fa fa-share"> خـوخــه </i> </p> <i class="fa fa-comment"></i> <i class="fa fa-share">  </i> </p> <i class="fa fa fa-comment"> </i> <i class="fa fa-share"> </i> </div></div><center></div></div><center><center><div><marquee direction="right" width="99%" id="asim7" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="2" >
