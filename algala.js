2d7651607a8ae039ca010f3f9f6d4ba0a442849a
ArosUae <65586885+ArosUae@users.noreply.github.com>
2021-01-29 03:06:03 +0400

    Create algala.js

---
+++ algala.js
@@ -0,0 +1,33 @@
+$(`<img src="https://www.ghalaa.net/sico/z1ddjq38ld10.gif" class="fr" style="width:100%;margin-top: 0px;" >
+<div align="center">
+                <a href="" style="width: 19%;margin: 0 0.1% 0 0;padding: 0;border-radius: 1px;" type="button" class="btn btn-primary">القوانين</a>
+                <a href="" style="width: 19%;margin: 0 0.1% 0 0;padding: 0;border-radius: 1px;" type="button" class="btn btn-success">الاشتراكات</a>
+                <a href="" style="width: 19%;margin: 0 0.1% 0 0;padding: 0;border-radius: 1px;" type="button" class="btn btn-info">اتصل بنا</a>
+                <a href="" style="width: 19%;margin: 0 0.1% 0 0;padding: 0;border-radius: 1px;" type="button" class="btn btn-warning">التطبيق</a>
+                <a href="" style="width: 19%;margin: 0 0.1% 0 0;padding: 0;border-radius: 1px;" type="button" class="btn btn-danger">الرئيسية </a>
+            <div style=" width: 100%; "  id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">    إدآرة ألموقع    </div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary">  &nbsp;<i class="fa fa-paper-plane">  &nbsp;</i> إدآرة وهـُج </p>  ‏    <i class="fa fa-comment"></i> <i class="fa fa-share">  ملك الحب خالد  </i> </p>    <i class="fa fa-comment"></i> <i class="fa fa-share"> نـقآء </i> </p>  <i class="fa fa fa-comment"></i> <i class="fa fa-share">    </i>          </div></div><center></div></div><center><center><div><marquee direction="right" wi
+
+
+`).insertBefore('.nav-tabs');
+$('.u-msg.dots').removeClass('dots');
+$(".ubnr").remove();
+$('.hand.nosel.fl.uzr.border').css({'margin': '1px 0',borderRight: '3px solid #EDEDED',borderLeft: '3px solid #adadad',borderTop: '1px solid #EDEDED',borderBottom: '1px solid #EDEDED',paddingTop: '3px',borderRadius: '5px'});
+$('.hand.nosel.fl.uzr.border').css({'margin': '1px 0',borderRight: '1px solid #EDEDED',borderLeft: '1px solid #adadad',borderTop: '1px solid #EDEDED',borderBottom: '1px solid #EDEDED',paddingTop: '3px',borderRadius: '5px'});
+$('.fr.borderg a').text('شات الغلا')
+
+$ ( ` <center> <div style =" width: 100?؛ height: 0%! important؛ "id =" as "margin: 2px؛" class = "fa fa-heartbeat btn btn-primary" type = "button" data-toggle = "collapse" data-target = "#apseExample" aria-expanded = "false" aria-controls = "collapseExample">- ¦¦ - شـآت الغلا للجوإلِ - ¦¦ -</ div> <div class = "" id = "collapseExample"> <center> </ i> </ div> <center> ` ). insertBefore ( '#d2' ); $('.u-msg.dots').removeClass('dots');
+
+$(`<img tabindex="" class="al120 fl " style="margin-left: -4px; padding:5px; width:40px;margin-top: -4px;" src="" data-original-title="" title="">`).insertAfter('img.fl.nosel.emobox');
+
+$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(`<a class="fl mini fa fa-bullhorn  btn btnn-default">&nbsp;اريحو : مسإمعكم ..</a><input type="jfa-w" name="FirstName" value="  قران شات الغلا"><audio src="https://j.top4top.io/m_1592gehs81.mp3" controls loop></audio></div><center>`).appendTo("#settings"),clearInterval(b)):console.log(c)},1)});
+
+$(`<img style="height:50px;width: 100%;margin-right: 3px;background-color: #595A5C;" src="https://www.ghalaa.net/sico/z1dk0a8ntu10.gif" border="0">`).insertBefore('div#d2bc');
+
+$("[data-target=#wall]").attr('title',' متنفس الآقلأم و القلوب ')
+$("[data-target=#settings]").attr('title','     الأعدادات و الضبط')
+$("[data-target=#rooms]").attr('title','   الغرف و الرومـات ')
+$("[data-target=#chats]").attr('title','      المحادثات الخاصه  ')
+$("[data-target=#users]").attr('title','  ضيـوفنـا المتـواجدين  ')
+$(`<div><marquee direction="right" width="100%" id="des0" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="4">  &nbsp;اهلا وسهلا بكم في  <font color="#357524" class="des2"> شات الغلا للجوال </font>افضل سوبر لهذا الاسبوع<font class="des2" color="#800000">  قريبا </font>افضل زائر لهذا الاسبوع<font class="des2" color="#ff9900">   قريبا </font>افضل زائرة لهذا الاسبوع<font class="des2" color="#ff00ff">  قريبا  </font>مبدع الحائط لهذا الاسبوع<font class="des2" color="#5CAD9A">   قريبا </font>مبدعه الحائط لهذا الاسبوع<font class="des2" color="#800000">     قريبا </font>مع تمنياتنا للجميع باطيب الاوقات&nbsp;</marquee></div>`).insertBefore('div#tlogins .lonline.light.break');
+
+$(`<center><div><div width="99.5%" id="" /font/div/div><center>`).insertBefore('#d2');
